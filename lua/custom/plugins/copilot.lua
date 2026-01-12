7849a444b63cedb432a0ab81a9474c96a1cad79c
Piotr Durniat <piotr@durniat.com>
2026-01-12 17:24:11 +0100

    updated gitsigns, and copilot setup

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -14,6 +14,7 @@ return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     branch = 'main',
+    build = 'make tiktoken',
     dependencies = {
       { 'zbirenbaum/copilot.lua' },
       { 'nvim-lua/plenary.nvim' },
@@ -22,48 +23,23 @@ return {
       debug = false,
       show_help = false,
       window = {
-        layout = 'float', -- (float) or 'vertical' (side panel)
-        width = 0.6, -- width (60% window width)
+        layout = 'float',
+        width = 0.6,
         height = 0.6,
       },
     },
     keys = {
-      {
-        '<leader>cc',
-        ':CopilotChatToggle<cr>',
-        mode = { 'n', 'v' },
-        desc = '[C]opilot [C]hat Toggle',
-      },
-
-      {
-        '<leader>ce',
-        ':CopilotChatExplain<cr>',
-        mode = 'v',
-        desc = '[C]opilot [E]xplain selection',
-      },
-      {
-        '<leader>cf',
-        ':CopilotChatFix<cr>',
-        mode = 'v',
-        desc = '[C]opilot [F]ix bugs in selection',
-      },
-      {
-        '<leader>co',
-        ':CopilotChatOptimize<cr>',
-        mode = 'v',
-        desc = '[C]opilot [O]ptimize selection',
-      },
+      { '<leader>cc', ':CopilotChatToggle<cr>', mode = { 'n', 'v' }, desc = '[C]opilot [C]hat Toggle' },
+      { '<leader>ce', ':CopilotChatExplain<cr>', mode = 'v', desc = '[C]opilot [E]xplain selection' },
+      { '<leader>cf', ':CopilotChatFix<cr>', mode = 'v', desc = '[C]opilot [F]ix bugs in selection' },
+      { '<leader>co', ':CopilotChatOptimize<cr>', mode = 'v', desc = '[C]opilot [O]ptimize selection' },
 
       {
         '<leader>cq',
         function()
           local input = vim.fn.input 'Prompt: '
           if input ~= '' then
-            require('CopilotChat').ask(input, { selection = require('CopilotChat.select').visual or nil })
-
-            -- require("CopilotChat").ask(input, { selection = function(source) return source.visual end })
-            -- require("CopilotChat").ask(input, { selection = require("CopilotChat.select").visual })
-            -- require("CopilotChat").ask(input, { selection = require("CopilotChat.select").visual })
+            require('CopilotChat').ask(input, {})
           end
         end,
         mode = { 'n', 'v' },
